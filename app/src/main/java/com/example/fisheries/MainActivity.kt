9032b9df70aaad438798306aef500a2c8f0b58f8
Nababkhan007 <markohenry46@gmail.com>
2019-10-16 16:21:31 +0600

    spinned added

--- app/src/main/java/com/example/fisheries/MainActivity.kt
+++ app/src/main/java/com/example/fisheries/MainActivity.kt
@@ -2,7 +2,8 @@ package com.example.fisheries
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -12,9 +13,28 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var option : Spinner
+    lateinit var result : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        option = findViewById(R.id.spinner_productTypeId) as Spinner
+        result = findViewById(R.id.tv_resultId) as TextView
+
+        var options = arrayOf("A", "B", "C")
+        option.adapter = ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, options)
+
+        option.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                result.text = "Please select an option"
+            }
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                result.text = options.get(p2)
+            }
+        }
 
         btn_postId.setOnClickListener {
             var trueValue: Boolean = true;
