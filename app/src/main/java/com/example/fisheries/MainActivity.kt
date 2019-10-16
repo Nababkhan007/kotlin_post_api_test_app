e24e82775668336b56303fac8ef39d07fed59d63
Nababkhan007 <markohenry46@gmail.com>
2019-10-16 14:37:03 +0600

    comment code erased

--- app/src/main/java/com/example/fisheries/MainActivity.kt
+++ app/src/main/java/com/example/fisheries/MainActivity.kt
@@ -17,21 +17,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btn_postId.setOnClickListener {
-            var trueValue:Boolean = true;
-            var title:String = et_titleId.text.toString();
-            var slug:String = et_slugId.text.toString();
-            var productType:Int = 1;
-
-            /*val BASE_URL = "http://192.168.10.20:8665/api/"
-
-            val retrofit = Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-            val getProducts = retrofit.create(ApiClient::class.java)*/
+            var trueValue: Boolean = true;
+            var title: String = et_titleId.text.toString();
+            var slug: String = et_slugId.text.toString();
+            var productType: Int = 1;
 
             RetrofitClient.instance.productDetailsPost(trueValue, title, slug, productType)
-                .enqueue(object: Callback<DefaultResponse>{
+                .enqueue(object : Callback<DefaultResponse> {
                     override fun onFailure(call: Call<DefaultResponse>, t: Throwable) {
                         Toast.makeText(applicationContext, t.message, Toast.LENGTH_LONG).show()
                     }
@@ -40,25 +32,11 @@ class MainActivity : AppCompatActivity() {
                         call: Call<DefaultResponse>,
                         response: Response<DefaultResponse>
                     ) {
-                        Toast.makeText(applicationContext, response.message(), Toast.LENGTH_LONG).show()
+                        Toast.makeText(applicationContext, response.message(), Toast.LENGTH_LONG)
+                            .show()
                     }
 
                 })
-
-            /*getProducts.productDetailsPost(trueValue, title, slug, productType)
-                .enqueue(object: Callback<DefaultResponse>{
-                    override fun onFailure(call: Call<DefaultResponse>, t: Throwable) {
-                        Toast.makeText(applicationContext, t.message, Toast.LENGTH_LONG).show()
-                    }
-
-                    override fun onResponse(
-                        call: Call<DefaultResponse>,
-                        response: Response<DefaultResponse>
-                    ) {
-                        Toast.makeText(applicationContext, response.message(), Toast.LENGTH_LONG).show()
-                    }
-
-                })*/
         }
     }
 }
